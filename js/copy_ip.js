1761dfdf3c3e710b3127668c925f3f06542a3ddd
Vianpyro <10519369+Vianpyro@users.noreply.github.com>
2019-07-28 13:06:08 -0400

    patch 0.0.3
    + Button changes to say that text have been copied to clipboard

--- js/copy_ip.js
+++ js/copy_ip.js
@@ -1,14 +1,22 @@
-function copyIp() {
-    var copyText = document.getElementById("ip");
-    copyText.select();
-    document.execCommand("copy");
-    clearSelection();
-}
-
 function clearSelection() {
     if (window.getSelection) {
         window.getSelection().removeAllRanges();
     } else if (document.selection) {
         document.selection.empty();
     }
+}
+
+function copyIp() {
+    var copyText = document.getElementById("ip");
+    copyText.select();
+    document.execCommand("copy");
+    clearSelection();
+
+    var ipButton = document.getElementById("ipButton");
+    ipButton.innerHTML = "Copied: " + copyText.value;
+}
+
+function outIp() {
+    var ipButton = document.getElementById("ipButton");
+    ipButton.innerHTML = "Copy";
 }
