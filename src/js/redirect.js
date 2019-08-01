f1e9f64a6179b547749320c9b4fec9bfc1d310c8
Vianpyro <10519369+Vianpyro@users.noreply.github.com>
2019-08-01 19:48:02 -0400

    patch 0.2.6
    - Refreshing forever on Firefox
    - Spelling mistake in french "rules".

--- src/js/redirect.js
+++ src/js/redirect.js
@@ -1,13 +1,12 @@
 let sysLang = navigator.language; // Récupère la langue du navigateur
-let meta = document.head.querySelector('[http-equiv="refresh"]'); // récupère le meta qui nous intéresse
 
 switch (sysLang) {
     case "en-US":
     case "fr-CA":
     case "fr-FR":
-        meta.content += `./${sysLang}/index.html`; // Ajoute du text dans le content du meta
+        window.location = `./${sysLang}/index.html`;
         break;
     default:
-        meta.content += `./en-US/index.html`;
+        window.location = `./en-US/index.html`;
         break;
 }
