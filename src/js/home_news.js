4e25a926c796f0c8355c48d9c6276a94fe5f41a3
Vianpyro <vianney.veremme@gmail.com>
2019-08-12 17:35:08 -0400

    Added news translations

--- src/js/home_news.js
+++ src/js/home_news.js
@@ -1,5 +1,9 @@
 let MAX_NB_NEWS = 20;
 
+let translateLang = navigator.language.slice(0, 2) || navigator.userLanguage.slice(0, 2) || "";
+let translationList = ["en", "fr"];
+let isTranslationAvailable = translationList.includes(translateLang);
+
 function getDate() {
     let d = new Date(),
         month = "" + (d.getMonth() + 1),
@@ -21,13 +25,26 @@ async function updateNews() {
         const data = new Request(`./src/json/news.json`);
         const { news } = await fetch(data).then(data => data.json());
         news.slice(0, MAX_NB_NEWS).forEach((element, i) => {
+            console.log(translateLang);
             if (element.available === "true") {
                 if (`${element.date}` === getDate()) {
-                    document.getElementById("news").innerHTML += `<figure id='data_news_${i}of${MAX_NB_NEWS}'><a href='${element.href}' target='${element.target}'><img src='${element.image}' class='${element.image_type}'>
-                    <figcaption><strong>New!</strong> ${element.title}</figcaption></a></figure>`;
+                    switch (translateLang) {
+                        case "fr":
+                            document.getElementById("news").innerHTML += `<figure id='data_news_${i}of${MAX_NB_NEWS}'><a href='${element.href}' target='${element.target}'><img src='${element.image}' class='${element.image_type}'>
+                                <figcaption><strong>New!</strong> ${element.title.fr}</figcaption></a></figure>`;
+                        default:
+                            document.getElementById("news").innerHTML += `<figure id='data_news_${i}of${MAX_NB_NEWS}'><a href='${element.href}' target='${element.target}'><img src='${element.image}' class='${element.image_type}'>
+                                <figcaption><strong>New!</strong> ${element.title.en}</figcaption></a></figure>`;
+                    }
                 } else {
-                    document.getElementById("news").innerHTML += `<figure id='data_news_${i}of${MAX_NB_NEWS}'><a href='${element.href}' target='${element.target}'><img src='${element.image}' class='${element.image_type}'>
-                    <figcaption>${element.title}</figcaption></a></figure>`;
+                    switch (translateLang) {
+                        case "fr":
+                            document.getElementById("news").innerHTML += `<figure id='data_news_${i}of${MAX_NB_NEWS}'><a href='${element.href}' target='${element.target}'><img src='${element.image}' class='${element.image_type}'>
+                                <figcaption>${element.title.fr}</figcaption></a></figure>`;
+                        default:
+                            document.getElementById("news").innerHTML += `<figure id='data_news_${i}of${MAX_NB_NEWS}'><a href='${element.href}' target='${element.target}'><img src='${element.image}' class='${element.image_type}'>
+                                <figcaption>${element.title.en}</figcaption></a></figure>`;
+                    }
                 }
             }
         });
